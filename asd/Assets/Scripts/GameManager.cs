636ee5b9a32ba812e92579e264d211696805ccb4
Scientist8 <cankaraca94@gmail.com>
2021-06-18 11:34:56 +0300

    Adding the game

---
+++ asd/Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour
+{
+    bool gameHasEnded = false;
+    public float RestartDelay = 1f;
+
+    public void Endgame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game over");
+            Invoke("Restart", RestartDelay);
+        }
+    
+        
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ asd/Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+    public GameManager gameManager;
+
+    void OnCollisionEnter (Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+            FindObjectOfType<GameManager>().Endgame();
+        } 
+
+    }
+}
